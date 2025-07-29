8c5be9ba567ecb2dc53a6d76d5bd324948326f6d
ElasticBottle <winstonyeo99@yahoo.com>
2025-07-29 14:47:28 -0400

    feat(core): update wait tool to take a screenshot after it's done waiting

--- packages/core/src/tools/wait.ts
+++ packages/core/src/tools/wait.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 import type { Tool } from ".";
 import { createAgentLogUpdate } from "../utils/agent-log";
+import type { ComputerProvider } from "./computer";
 
 const waitToolSchema = z.object({
   duration: z
@@ -13,7 +14,11 @@ const waitToolSchema = z.object({
 
 export type WaitToolArgs = z.infer<typeof waitToolSchema>;
 
-export function createWaitTool(): Tool<typeof waitToolSchema> {
+export function createWaitTool<T, R>({
+  computerProvider,
+}: {
+  computerProvider: ComputerProvider<T, R>;
+}): Tool<typeof waitToolSchema> {
   return {
     description:
       "Wait for a certain duration. Normally used to wait for a page to load, an animation to complete, or a certain task/action to complete.",
@@ -21,12 +26,25 @@ export function createWaitTool(): Tool<typeof waitToolSchema> {
     execute: async (args, context) => {
       await new Promise((resolve) => setTimeout(resolve, args.duration * 1000));
 
+      const screenshot = await computerProvider.takeScreenshot(
+        context.sessionId,
+      );
+      const screenshotUrl = await computerProvider.uploadScreenshot?.({
+        screenshotBase64: screenshot,
+        sessionId: context.sessionId,
+        step: context.step,
+      });
+
       const response = {
         role: "user" as const,
         content: [
           {
             type: "text" as const,
-            text: `Waited for ${args.duration} seconds. Reason: ${args.reasoning}`,
+            text: `Waited for ${args.duration} seconds. Reason: ${args.reasoning}. Screenshot as attached.`,
+          },
+          {
+            type: "image" as const,
+            image: screenshotUrl?.url ? new URL(screenshotUrl.url) : screenshot,
           },
         ],
       };
@@ -39,7 +57,24 @@ export function createWaitTool(): Tool<typeof waitToolSchema> {
           toolName: "wait",
           args,
           reasoning: args.reasoning,
-          response,
+          response: {
+            role: "user",
+            content: response.content.map((c) => {
+              if (c.type === "text") {
+                return c;
+              }
+              return {
+                type: "image",
+                image:
+                  screenshotUrl?.url ?? "[screenshot removed to preserve size]",
+              };
+            }),
+          },
+          screenshot: {
+            value:
+              screenshotUrl?.url ?? "[screenshot removed to preserve size]",
+            overrideLogScreenshot: true,
+          },
         }),
       };
     },
